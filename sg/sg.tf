e31bbcb3a7488d5bb9c31d0d57c8834c0a305521
lazycoder95 <devas.5991@gmail.com>
2024-11-22 00:38:17 +0530

    Securitygroup and eip

---
+++ sg/sg.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_security_group" "terraform-sg" {
+  name        = "for terraform training"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.terraform-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.terraform-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
